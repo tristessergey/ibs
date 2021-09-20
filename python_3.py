90a8019d150e264bdd31067dd03b8f46b0e3d91e
tristessergey <75569384+tristessergey@users.noreply.github.com>
2021-09-20 15:20:14 +0300

    Delete python_3.py

--- python_3.py
+++
@@ -1,8 +0,0 @@
-numbers = [0, 99, 100, 53, 44, 23, 4, 8, 16, 15, 77, 51]
-def filter_even_num(i):
-    if(i % 2) == 1 and i >= 50:
-        return True
-    else:
-        return False
-f = filter(filter_even_num, numbers)
-print(list(f))
