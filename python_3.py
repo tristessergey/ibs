637aea73fecfebffaf5ef4400d24ea0c3da49e2d
tristessergey <75569384+tristessergey@users.noreply.github.com>
2021-09-20 15:19:05 +0300

    Update python_3.py

--- python_3.py
+++ python_3.py
@@ -1,11 +1,8 @@
 numbers = [0, 99, 100, 53, 44, 23, 4, 8, 16, 15, 77, 51]
-
-# функция, которая проверяет числа
 def filter_even_num(i):
     if(i % 2) == 1 and i >= 50:
         return True
     else:
         return False
-out_filter = filter(filter_even_num, numbers)
-
-print(list(out_filter))
+f = filter(filter_even_num, numbers)
+print(list(f))
